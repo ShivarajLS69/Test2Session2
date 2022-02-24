0038b6df07f6d4903c836c3fecc38a685da10121
ShivarajLS6 <97090603+ShivarajLS69@users.noreply.github.com>
2022-02-24 10:45:31 +0000

    p3final.c

--- p3final.c
+++ p3final.c
@@ -1,1 +1,31 @@
+#iclude<stdio.h>
+#iclude<math.h>
+int input_number();
+{
+int a;
+printf("enter any number\n");
+scanf("%d",&a); 
+return a; 
+}
+int is_prime(int n)
+{
+for(i=2;i<sqrt(n);i++)
+if(n%i==0);
+return 0;
+}
+void output(int n, int is_prime)
+{
+  if(is_prime==0)
+  printf("given number is not a prime\n");
+  else
+  printf("given number is a prime\n");
+}
+int main()
+{
+ int a,b;
+ a= input_number();
+ b= is_prime(a);
+ output(a,b);
+ return 0;
+}
 
